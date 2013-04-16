bcbb705976e4f483ee4a967aa1df7acdc40ec4e4
Jarl Erik Schmidt <github@jarlerik.com>
2013-04-16 18:56:53 +0200

    Added AccountStats payload type

---
+++ Winterday.External.Gengo/Payloads/AccountStats.cs
@@ -0,0 +1,64 @@
+//
+// AccountStats.cs
+//
+// Author:
+//       Jarl Erik Schmidt <github@jarlerik.com>
+//
+// Copyright (c) 2013 Jarl Erik Schmidt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+namespace Winterday.External.Gengo.Payloads
+{
+	using System;
+	using System.Xml.Linq;
+
+	public class AccountStats
+	{
+		readonly decimal _creditsSpent;
+		readonly DateTime _userSince;
+
+		public decimal CreditsSpent {
+			get {
+				return _creditsSpent;
+			}
+		}
+
+		public DateTime UserSince {
+			get {
+				return _userSince;
+			}
+		}
+
+		AccountStats (decimal creditsSpent, DateTime userSince)
+		{
+			_creditsSpent = creditsSpent;
+			_userSince = userSince;
+		}
+
+		internal static AccountStats FromXContainer(XContainer c) {
+			
+			return new AccountStats(
+				c.Element ("credits_spent").Value.ToDecimal(),
+				c.Element ("user_since").Value.ToLong().ToDateFromTimestamp()
+				);
+			
+		}
+	}
+}
+
