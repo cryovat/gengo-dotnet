d8003c173f7e2eac060338eb536ad604eb1914b2
Jarl Erik Schmidt <github@jarlerik.com>
2013-05-14 01:25:43 +0200

    Moved usings into namespaces/added linebreak

--- Winterday.External.Gengo/GengoClient.cs
+++ Winterday.External.Gengo/GengoClient.cs
@@ -23,12 +23,11 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-using System.Collections.Generic;
-
 
 namespace Winterday.External.Gengo
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Net;
     using System.Net.Http;
